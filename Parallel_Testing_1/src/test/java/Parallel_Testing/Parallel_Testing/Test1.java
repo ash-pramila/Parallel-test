e9cc5fd0897090d92f5632f8b88f1d9cd3db1747
Pramila Palavesam Raju <pramilap0123@gmail.com>
2018-09-25 16:54:26 +0530

    Initial commit

---
+++ Parallel_Testing_1/src/test/java/Parallel_Testing/Parallel_Testing/Test1.java
@@ -0,0 +1,34 @@
+package Parallel_Testing.Parallel_Testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	private WebDriver driver; 
+	String appURL = "http://google.com";
+
+	@BeforeClass
+	public void testSetUp() {
+		System.setProperty("webdriver.chrome.driver","D:\\driver\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void verifyGooglePageTittle() {
+		driver.navigate().to(appURL);
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "Google");
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	
+}
